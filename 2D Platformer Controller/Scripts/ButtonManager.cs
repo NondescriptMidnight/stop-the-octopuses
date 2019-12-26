60c21ddd5bcef35328139b0e0d5bfc7b85fb5a40
Stacey-Eva Fabrè <59258519+NondescriptMidnight@users.noreply.github.com>
2019-12-27 00:34:27 +0200

    Prefabs, scenes, 2d plaftormer controller

---
+++ 2D Platformer Controller/Scripts/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonManager : MonoBehaviour
+{ 
+
+    // Use this for initialization
+    public void Repeat()
+    {
+        Application.LoadLevel(0);
+    }
+    public void Continue()
+    {
+        Application.Quit();
+    }
+}
